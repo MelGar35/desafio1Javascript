dab2b66af016b6e9c743c6c3597b488afdb3f0a0
Melisa Garcia <melisagarciadeveloper@gmail.com>
2022-07-20 17:16:34 -0300

    desafio1Javascript

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+
+let nombre = prompt("Ingresa tu nombre")
+alert("Bienvenida " + nombre + "!")
+
+
+let edad = prompt ("Cuantos años tenes? ingresá tu edad!")
+
+if(edad>=18) {
+    alert ("Tienes 18 años o más, puedes ingresar")
+    prompt("Que bebida deseas comprar?. Escribe el numero de la opción 1.Vodka 2.Ron 3.Fernet")
+}else if(edad<18) {
+    alert ("Auch, tienes menos de 18 años, no puedes ingresar!")
+} 
+
+
+
+
+
+
